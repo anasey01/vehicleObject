fed74382636db8b3c29c66a2a719fd3ae766a627
Oluseyi Anani <anasey2018@gmail.com>
2018-07-23 08:39:19 +0100

    vehicle attributes added.

---
+++ vehicle.js
@@ -0,0 +1,15 @@
+/*
+    Assignment:  Create a class of vehicle. That class should have at least 5 attribute and 5 methods.
+    Test that the class and attributes are working.
+*/
+
+class Vehicle{
+    constructor(color, make, model, year, numberOfWheels){
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.numberOfWheels = numberOfWheels;
+    }
+
+}
